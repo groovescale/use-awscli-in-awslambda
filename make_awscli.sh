e613f15f320afde057d15d378142c46aa38fb3bd
Jeff Henrikson <jeff@groovescale.com>
2017-04-30 21:11:48 -0700

    version 1.11.82

--- make_awscli.sh
+++ make_awscli.sh
@@ -13,7 +13,7 @@ fi
 
 adirVenv=$(mktemp -d /tmp/lambda-XXXXXX)
 virtualenv $adirVenv
-$adirVenv/bin/pip install awscli
+$adirVenv/bin/pip install awscli==1.11.82
 perl -pi -e '$_ ="#!/usr/bin/python\n" if $. == 1' $adirVenv/bin/aws
 rm -f $afileZip
 (cd $adirVenv/lib/python2.7/site-packages && zip -q -r $afileZip *)
